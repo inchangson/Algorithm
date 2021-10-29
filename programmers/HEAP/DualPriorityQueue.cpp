d850d7f5426e84be14043d2c0ad0037ebf269b19
inchangson <3721cha21.11@gmail.com>
2021-10-30 00:34:18 +0900

    programmers heap dual priorityqueue ver 2

--- programmers/HEAP/DualPriorityQueue.cpp
+++ programmers/HEAP/DualPriorityQueue.cpp
@@ -14,18 +14,23 @@ struct node {
     bool operator < (const node& n) const{
         return value < n.value;
     }
+    bool operator > (const node& n) const{
+        return value > n.value;
+    }
 };
 
 bool isExist[MX_SZ];
-priority_queue<node, vector<node>, greater<>> maxQueue;
-priority_queue<node, vector<node>, less<>> minQueue;
+priority_queue<node, vector<node>, greater<>> maxHeap;
+priority_queue<node, vector<node>, less<>> minHeap;
+int heapSize;
 
 void initData(int dataSize){
     for(int idx = 0; idx < dataSize; ++idx){
         isExist[idx] = 0;
     }
-    maxQueue = priority_queue<node, vector<node>, greater<>>();
-    minQueue = priority_queue<node, vector<node>, less<>>();
+    maxHeap = priority_queue<node, vector<node>, greater<>>();
+    minHeap = priority_queue<node, vector<node>, less<>>();
+    heapSize = 0;
 }
 
 vector<string> parsingString(string str)
@@ -38,10 +43,76 @@ vector<string> parsingString(string str)
     return rslt;
 }
 
-int executeOperation(string executionType, string executionVal){
-    int val = stoi(executionVal);
-    cout << executionType << " " << val << endl;
-    return 0;
+void pushData(int val){
+    cout << "\n======pushData Start======\n";
+    ++heapSize;
+    isExist[heapSize] = true;
+    minHeap.push({heapSize, val});
+    maxHeap.push({heapSize, val});
+    
+    cout << heapSize << " " << val << endl;    
+    cout << "======pushData End======\n";
+
+}
+
+int popMax(bool isSucceed){
+    int retVal = -1;
+    isSucceed = false;
+    while(isExist[maxHeap.top().index] && !maxHeap.empty()){
+        maxHeap.pop();    
+    }
+    if(maxHeap.empty()) return retVal;
+    isSucceed = true;
+    isExist[maxHeap.top().index] = false;
+    retVal = maxHeap.top().value;
+    maxHeap.pop();
+    return retVal;
+}
+
+int popMin(bool isSucceed){
+    int retVal = -1;
+    isSucceed = false;
+    while(isExist[minHeap.top().index] && !minHeap.empty()){
+        minHeap.pop();    
+    }
+    if(minHeap.empty()) return retVal;
+    isSucceed = true;
+    isExist[minHeap.top().index] = false;
+    retVal = minHeap.top().value;
+    minHeap.pop();
+    return retVal;
+}
+
+void executeOperation(string executionType, string executionVal){
+    if(executionType == "I"){
+        pushData(stoi(executionVal));
+    }else if(executionVal == "1"){
+        bool isSucceed;
+        popMax(isSucceed);
+    }else{
+        bool isSucceed;
+        popMin(isSucceed);
+    }
+}
+
+void getAnswer(vector<int> & answer){
+    int min;
+    int max;
+    bool isSucceed1;
+    bool isSucceed2;
+    min = popMin(isSucceed1);
+    max = popMax(isSucceed2);
+    
+    if(isSucceed1 && isSucceed2){
+        answer.push_back(min);
+        answer.push_back(max);
+    }else if(isSucceed1 && !isSucceed2){
+        answer.push_back(min);
+        answer.push_back(min);
+    }else{
+        answer.push_back(0);
+        answer.push_back(0);
+    }
 }
 
 vector<int> solution(vector<string> operations) {
@@ -52,8 +123,9 @@ vector<int> solution(vector<string> operations) {
     vector<string> operation;
     for(int idx = 0; idx < dataSize; ++idx){
         operation = parsingString(operations[idx]);
-        answer.push_back(executeOperation(operation[0], operation[1]));
+        executeOperation(operation[0], operation[1]);
     }
+    getAnswer(answer);
     
     return answer;
 }
