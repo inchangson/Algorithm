2516a24adc1316cb99e91d181bca68672696ee78
inchangson <3721cha21.11@gmail.com>
2021-12-07 23:16:30 +0900

    msp tree prob basic solving with kruskal algorithm

---
+++ boj/boj_1922.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+constexpr int MX_N = 1000;
+constexpr int MX_M = 100000;
+constexpr int NODE1 = 0;
+constexpr int NODE2 = 1;
+constexpr int COST = 2;
+
+int n;
+int m;
+int costs[MX_M][3];
+int parents[MX_N + 1];
+int ans;
+
+bool myCmp(int* a, int* b){
+	return a[2] < b[2];
+}
+
+void initData(){
+	ans = 0;
+	//make parents arr
+	for(int idx = 0; idx < n; ++idx){
+		parents[idx] = idx;
+	}
+}
+
+void inputData(){
+	scanf("%d %d", &n, &m);
+	
+	for(int idx = 0; idx < m; ++idx){
+		scanf("%d %d %d", &costs[idx][NODE1], &costs[idx][NODE2], &costs[idx][COST]);	
+	}
+}
+
+int getParent(int child){
+	if(parents[child] = child)	return child;
+	else return getParent(parents[child]);
+}
+
+void solve(){
+	//sort(costs, costs + m, myCmp);
+	
+	int totalCosts = 0;
+	for(int idx = 0; idx < m; ++idx){
+		int parentA = getParent(costs[idx][0]);
+		int parentB = getParent(costs[idx][1]);
+		if(parentA == parentB)	continue;
+		totalCosts += costs[idx][2];
+		
+		if(parentA < parentB){
+			parents[parentB] = parentA;
+		}
+		else{
+			parents[parentA] = parentB;
+		}
+	}
+	ans = totalCosts;
+}
+
+int main(){
+	initData();
+	inputData();
+	solve();
+	printf("%d\n", ans);	
+	return 0;
+}
