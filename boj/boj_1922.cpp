bd4c6eb6430c266fd2b2938a9267191aa0c48bac
inchangson <3721cha21.11@gmail.com>
2021-12-10 21:53:58 +0900

    boj 1922 solved with kruskal

--- boj/boj_1922.cpp
+++ boj/boj_1922.cpp
@@ -6,57 +6,58 @@
 
 using namespace std;
 
+struct Network{
+	int from;
+	int to;
+	int cost;
+};
+
+constexpr int INF = 0X7FFFFFFF;
 constexpr int MX_N = 1000;
 constexpr int MX_M = 100000;
-constexpr int NODE1 = 0;
-constexpr int NODE2 = 1;
-constexpr int COST = 2;
 
-int n;
-int m;
-int costs[MX_M][3];
+int N;
+int M;
+Network networks[MX_M];
 int parents[MX_N + 1];
-int ans;
-
-void initData(){
-	ans = 0;
-	//make parents arr
-	for(int idx = 0; idx < n; ++idx){
-		parents[idx] = idx;
-	}
-}
 
 void inputData(){
-	scanf("%d %d", &n, &m);
-	
-	for(int idx = 0; idx < m; ++idx){
-		scanf("%d %d %d", &costs[idx][NODE1], &costs[idx][NODE2], &costs[idx][COST]);	
+	scanf("%d %d", &N, &M);
+
+	for(int idx = 0; idx <= N; ++idx){
+		parents[idx] = idx;
+	}
+	for(int idx = 0; idx < M; ++idx){
+		scanf("%d %d %d", &networks[idx].from, &networks[idx].to, &networks[idx].cost);
+		if(networks[idx].from == networks[idx].to){
+			networks[idx].cost = INF;
+		}
 	}
 }
 
 int getParent(int child){
-	if(parents[child] = child)	return child;
+	if(parents[child] == child)	return child;
 	else return getParent(parents[child]);
 }
 
-void solve(){
-	//sort(costs, costs + m, myCmp);
-	
-	std::qsort(costs, 10, sizeof(*costs),
-        [](const void *arg1, const void *arg2)->int
-        {
-            int const *lhs = static_cast<int const*>(arg1);
-            int const *rhs = static_cast<int const*>(arg2);
-            return lhs[2] - rhs[2];
-        });
+bool myCmp(Network n1, Network n2){
+	return n1.cost < n2.cost;
+}
 
+int solve(){
+	sort(networks, networks + M, myCmp);
 	
-	int totalCosts = 0;
-	for(int idx = 0; idx < m; ++idx){
-		int parentA = getParent(costs[idx][0]);
-		int parentB = getParent(costs[idx][1]);
+	int totalCost = 0;
+	int endFlag = 0;
+	for(int idx = 0; idx < M; ++idx){
+		if(endFlag == N)    return totalCost;
+		
+		int parentA = getParent(networks[idx].from);
+		int parentB = getParent(networks[idx].to);
+
 		if(parentA == parentB)	continue;
-		totalCosts += costs[idx][2];
+		totalCost += networks[idx].cost;
+		endFlag++;
 		
 		if(parentA < parentB){
 			parents[parentB] = parentA;
@@ -65,13 +66,14 @@ void solve(){
 			parents[parentA] = parentB;
 		}
 	}
-	ans = totalCosts;
+	
+	return totalCost;
 }
 
 int main(){
-	initData();
+	
 	inputData();
-	solve();
-	printf("%d\n", ans);	
+	printf("%d\n", solve());
+	
 	return 0;
 }
