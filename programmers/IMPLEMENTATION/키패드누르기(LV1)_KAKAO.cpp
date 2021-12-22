c679016a175e13b6fdcca6819df9184b2c47cf95
inchangson <3721cha21.11@gmail.com>
2021-12-22 17:53:58 +0900

    프로그래머스 구현/ 키패드 누르기( kakao2020 lv1)

--- programmers/IMPLEMENTATION/키패드누르기(LV1)_KAKAO.cpp
+++ programmers/IMPLEMENTATION/키패드누르기(LV1)_KAKAO.cpp
@@ -34,49 +34,55 @@ void initData(){
     curR = {START_ROW, START_COL_R};
 }
 
+int getAbs(int n){
+    if(n < 0) return -n;
+    return n;
+}
+
 string solution(vector<int> numbers, string hand) {
     string answer = "";
     initData();
     
     bool isRightHanded = hand[0] == 'r';
     int keyR, keyC;
+    int distR, distL;
     for(int curNum : numbers){
         keyR = keyLookUp[curNum].r;
         keyC = keyLookUp[curNum].c;
         
         if(keyC == 0){
             answer += L;
-            curL = {keyR, keyC};
+            curL = keyLookUp[curNum];
+            continue;
         }
-        else if(keyC == 2){
+        if(keyC == 2){
             answer += R;
-            curR = {keyR, keyC};
-        }
-        else{
-            int distL = (keyR - curL.r) * (keyR - curL.r) +
-                        (keyC - curL.c) * (keyC - curL.c);
-            int distR = (keyR - curR.r) * (keyR - curR.r) +
-                        (keyC - curR.c) * (keyC - curR.c);
-            if(distL == distR){
-                if(isRightHanded){
-                    answer += R;
-                    curR = {keyR, keyC};
-                }
-                else{
-                    answer += L;
-                    curL = {keyR, keyC};
-                }
-            }
-            else if(distL < distR){
-                answer += L;
-                curL = {keyR, keyC};
-            }
-            else{
-                answer += R;
-                curR = {keyR, keyC};
-            }
+            curR = keyLookUp[curNum];
+            continue;
         }
         
+        distR = (getAbs(keyR - curR.r) + getAbs(keyC - curR.c));
+        distL = (getAbs(keyR - curL.r) + getAbs(keyC - curL.c));
+        
+        if(distL < distR){
+            answer += L;
+            curL = keyLookUp[curNum];
+            continue;
+        }
+        if(distL > distR){
+            answer += R;
+            curR = keyLookUp[curNum];
+            continue;
+        }
+        
+        if(isRightHanded){
+            answer += R;
+            curR = keyLookUp[curNum];
+        }
+        else{
+            answer += L;
+            curL = keyLookUp[curNum];
+        }        
     }
     
     return answer;
