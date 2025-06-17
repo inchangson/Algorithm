f99cc5b74cc8403976d7de6e00596ec8a00aa5fa
son-inchang <3721cha21.11@gmail.com>
2025-06-17 20:58:15 +0900

    [refactor] 프로그래머스_해시_01_java - getOrDefault 활용 - entrySet(), Map.Entry<K, V> 활용

--- programmers/HASH/완주하지_못한_선수.java
+++ programmers/HASH/완주하지_못한_선수.java
@@ -16,28 +16,51 @@ public class 완주하지_못한_선수  {
             decrementMap(c, result);
         }
         
-        for (String k : result.keySet()) {
-            answer = k;
+        for (Map.Entry<String, Integer> kV : result.entrySet()) {
+            if (kV.getValue() > 0)
+                answer = kV.getKey();
         }
         
         return answer;
     }
     
     private static void incrementMap(String k, Map<String, Integer> m) {
-        if (m.containsKey(k)) {
-            m.put(k, m.get(k) + 1);
-        } else {
-            m.put(k, 1);
-        }
+        m.put(k, m.getOrDefault(k, 0) + 1);
     }
     
     private static void decrementMap(String k, Map<String, Integer> m) {
-        int prev = m.get(k);
-        // assert prev <= 0
-        if (prev == 1) {
-            m.remove(k);
-        } else {
-            m.put(k, prev - 1);
-        }
+        m.put(k, m.get(k) - 1);
     }
 }
+// 내 답안
+// 채점을 시작합니다.
+// 정확성  테스트
+// 테스트 1 〉	통과 (0.07ms, 76.4MB)
+// 테스트 2 〉	통과 (0.09ms, 94.9MB)
+// 테스트 3 〉	통과 (0.45ms, 80.6MB)
+// 테스트 4 〉	통과 (0.71ms, 75.9MB)
+// 테스트 5 〉	통과 (0.80ms, 88.3MB)
+// 테스트 6 〉	통과 (0.04ms, 82.3MB)
+// 테스트 7 〉	통과 (0.10ms, 80.1MB)
+// 효율성  테스트
+// 테스트 1 〉	통과 (30.30ms, 84.8MB)
+// 테스트 2 〉	통과 (53.92ms, 89.5MB)
+// 테스트 3 〉	통과 (104.25ms, 97MB)
+// 테스트 4 〉	통과 (59.63ms, 96.6MB)
+// 테스트 5 〉	통과 (63.80ms, 102MB)
+
+// 변경 답안
+// 정확성  테스트
+// 테스트 1 〉	통과 (0.06ms, 85.1MB)
+// 테스트 2 〉	통과 (0.08ms, 74MB)
+// 테스트 3 〉	통과 (0.63ms, 88.7MB)
+// 테스트 4 〉	통과 (1.19ms, 75.9MB)
+// 테스트 5 〉	통과 (1.49ms, 78.5MB)
+// 테스트 6 〉	통과 (0.04ms, 74.2MB)
+// 테스트 7 〉	통과 (0.05ms, 71.1MB)
+// 효율성  테스트
+// 테스트 1 〉	통과 (49.10ms, 99.7MB)
+// 테스트 2 〉	통과 (69.39ms, 89.8MB)
+// 테스트 3 〉	통과 (65.03ms, 96.4MB)
+// 테스트 4 〉	통과 (71.99ms, 113MB)
+// 테스트 5 〉	통과 (76.07ms, 97.8MB)
