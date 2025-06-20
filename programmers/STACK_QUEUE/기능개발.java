410cea7769a4904de10e684843b278b30923b1fa
son-inchang <3721cha21.11@gmail.com>
2025-06-20 20:07:59 +0900

    [feat] 프로그래머스_스택큐_02_java - use LinkedList for deque i/f - offer(insert), poll(remove), peek(examine) + First/Last on suffix

--- programmers/STACK_QUEUE/기능개발.java
+++ programmers/STACK_QUEUE/기능개발.java
@@ -1,5 +1,33 @@
 package com.kt.aicare.common.util.programmers.STACK_QUEUE;
+import java.util.*;
 
 public class 기능개발 {
 
+  // p : progress, s: speed
+  private int getCurEndDay(int p, int s) {
+    int left = (100 - p);
+    if (left % s == 0) return left / s;
+    return (left / s) + 1;
+  }
+
+  public int[] solution(int[] progresses, int[] speeds) {
+    int[] answer = {};
+    int N = speeds.length;
+    LinkedList<Integer> deque = new LinkedList<>();
+
+    int preEndDay = 0;
+    for (int i = 0; i < N; i++) {
+      int curEndDay = getCurEndDay(progresses[i], speeds[i]);
+      if (preEndDay >= curEndDay) {
+        Integer cache = deque.pollLast();
+//        assert cache != null;
+        deque.addLast(cache + 1);
+      } else {
+        deque.addLast(1);
+        preEndDay = curEndDay;
+      }
+    }
+
+    return deque.stream().mapToInt(Integer::valueOf).toArray();
+  }
 }
