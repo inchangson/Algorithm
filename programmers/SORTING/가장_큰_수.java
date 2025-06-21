f8ce5e5ffa7c5348fb87f8ea9d30764f5e34f737
son-inchang <3721cha21.11@gmail.com>
2025-06-21 14:45:11 +0900

    [refactor] 프로그래머스_정렬_02_java - sorted() 사용, 성능엔 이점 없음

--- programmers/SORTING/가장_큰_수.java
+++ programmers/SORTING/가장_큰_수.java
@@ -5,14 +5,11 @@ import java.util.stream.Collectors;
 public class 가장_큰_수 {
   int[] getSortedArray(int[] src) {
     List<Integer> ret = Arrays.stream(src)
-        .boxed()
-        .collect(Collectors.toList());
-
-    ret.sort((a, b) -> {
-      String s1 = String.valueOf(a) + String.valueOf(b);
-      String s2 = String.valueOf(b) + String.valueOf(a);
-      return s2.compareTo(s1);
-    });
+        .boxed().sorted((a, b) -> {
+          String s1 = String.valueOf(a) + String.valueOf(b);
+          String s2 = String.valueOf(b) + String.valueOf(a);
+          return s2.compareTo(s1);
+        }).collect(Collectors.toList());
 
     return ret.stream().mapToInt(Integer::valueOf).toArray();
   }
@@ -48,3 +45,20 @@ public class 가장_큰_수 {
 //테스트 13 〉	통과 (4.60ms, 72.8MB)
 //테스트 14 〉	통과 (4.68ms, 74.2MB)
 //테스트 15 〉	통과 (4.70ms, 87.5MB)
+
+// 수정 답안 (sorted() 사용)
+//테스트 1 〉	통과 (230.31ms, 191MB)
+//테스트 2 〉	통과 (130.11ms, 138MB)
+//테스트 3 〉	통과 (290.27ms, 212MB)
+//테스트 4 〉	통과 (21.23ms, 94.6MB)
+//테스트 5 〉	통과 (195.77ms, 178MB)
+//테스트 6 〉	통과 (150.91ms, 156MB)
+//테스트 7 〉	통과 (5.65ms, 83.8MB)
+//테스트 8 〉	통과 (5.15ms, 88.2MB)
+//테스트 9 〉	통과 (5.02ms, 76.2MB)
+//테스트 10 〉	통과 (5.36ms, 86MB)
+//테스트 11 〉	통과 (4.89ms, 71.4MB)
+//테스트 12 〉	통과 (6.16ms, 89MB)
+//테스트 13 〉	통과 (4.57ms, 73.1MB)
+//테스트 14 〉	통과 (5.00ms, 72.9MB)
+//테스트 15 〉	통과 (4.77ms, 76.6MB)
