b4da53f05873fd86c59610b7494e86e35ebce63b
son-inchang <3721cha21.11@gmail.com>
2025-06-21 14:39:04 +0900

    [feat] 프로그래머스_정렬_01_java - java에선 숫자 타입에서만 <, > 사용 가능 - compareTo 사용할 것

---
+++ programmers/SORTING/가장_큰_수.java
@@ -0,0 +1,50 @@
+package com.kt.aicare.common.util.programmers.SORTING;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class 가장_큰_수 {
+  int[] getSortedArray(int[] src) {
+    List<Integer> ret = Arrays.stream(src)
+        .boxed()
+        .collect(Collectors.toList());
+
+    ret.sort((a, b) -> {
+      String s1 = String.valueOf(a) + String.valueOf(b);
+      String s2 = String.valueOf(b) + String.valueOf(a);
+      return s2.compareTo(s1);
+    });
+
+    return ret.stream().mapToInt(Integer::valueOf).toArray();
+  }
+  public String solution(int[] numbers) {
+    int[] sorted = getSortedArray(numbers);
+    StringBuffer sb = new StringBuffer();
+
+    if (sorted[0] == 0) {
+      sb.append(0);
+    } else {
+      for (int s : sorted) {
+        sb.append(s);
+      }
+    }
+
+    return sb.toString();
+  }
+
+}
+// 내 답안
+//테스트 1 〉	통과 (260.92ms, 180MB)
+//테스트 2 〉	통과 (155.54ms, 126MB)
+//테스트 3 〉	통과 (265.39ms, 236MB)
+//테스트 4 〉	통과 (18.72ms, 98.3MB)
+//테스트 5 〉	통과 (222.59ms, 150MB)
+//테스트 6 〉	통과 (163.42ms, 138MB)
+//테스트 7 〉	통과 (6.37ms, 87MB)
+//테스트 8 〉	통과 (6.53ms, 72.2MB)
+//테스트 9 〉	통과 (5.11ms, 72.1MB)
+//테스트 10 〉	통과 (4.88ms, 72.7MB)
+//테스트 11 〉	통과 (5.22ms, 86.2MB)
+//테스트 12 〉	통과 (8.03ms, 79.6MB)
+//테스트 13 〉	통과 (4.60ms, 72.8MB)
+//테스트 14 〉	통과 (4.68ms, 74.2MB)
+//테스트 15 〉	통과 (4.70ms, 87.5MB)
