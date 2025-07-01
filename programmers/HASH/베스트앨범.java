d53f5ba538fe6eb29aa802e5451d9f6678801427
son-inchang <3721cha21.11@gmail.com>
2025-07-01 09:45:24 +0900

    [refactor] 프로그래머스_해시_05_java - sorted, forEach 활용

--- programmers/HASH/베스트앨범.java
+++ programmers/HASH/베스트앨범.java
@@ -1,40 +1,33 @@
 package com.kt.aicare.common.util.programmers.HASH;
 import java.util.*;
-import java.util.stream.*;
 
 public class 베스트앨범 {
+    Map<String, Integer> genreMap = new HashMap<>();
+    Map<String, List<int[]>> songMap = new HashMap<>();
+
+    void initData(String[] genres, int[] plays) {
+        genreMap = new HashMap<>();
+        songMap = new HashMap<>();
+        for (int i = 0; i < genres.length; i++) {
+            genreMap.put(genres[i], genreMap.getOrDefault(genres[i], 0) + plays[i]);
+            songMap.computeIfAbsent(genres[i], k -> new ArrayList<>()).add(new int[]{i, plays[i]});
+        }
+        songMap.forEach((k, v) -> v.sort((o1, o2) -> o2[1] - o1[1]));
+    }
 
     public List<Integer> solution(String[] genres, int[] plays) {
         List<Integer> answer = new ArrayList<>();
 
-        Map<String, Integer> genreMap = new HashMap<>();
-        Map<String, List<int[]>> songMap = new HashMap<>();
+        initData(genres, plays);
 
-        for (int no = 0; no < genres.length; no++) {
-            String g = genres[no];
-            int p = plays[no];
-            genreMap.put(g, genreMap.getOrDefault(g, 0) + p);
-            if (!songMap.containsKey(g)) {
-                songMap.put(g, new ArrayList<int[]>());
-            }
-            songMap.get(g).add(new int[]{no, p});
-        }
+        genreMap.entrySet().stream()
+            .sorted((a, b) -> b.getValue() - a.getValue())
+            .forEach(entry -> {
+                List<int[]> songs = songMap.get(entry.getKey());
+                answer.add(songs.get(0)[0]);
+                if (songs.size() > 1) answer.add(songs.get(1)[0]);
+            });
 
-        List<Map.Entry<String, Integer>> genreList = genreMap.entrySet()
-            .stream()
-            .sorted((e1, e2) -> e2.getValue().compareTo(e1.getValue()))
-            .collect(Collectors.toList());
-
-        for (Map.Entry<String, Integer> genre : genreList) {
-            List<int[]> target = songMap.get(genre.getKey());
-            if (target.size() == 1) {
-                answer.add(target.get(0)[0]);
-                continue;
-            }
-            target.sort((o1, o2) -> {return o2[1] - o1[1];});
-            answer.add(target.get(0)[0]);
-            answer.add(target.get(1)[0]);
-        }
         return answer;
     }
 }
