fc96b24132665ad9d8b451276702ecbfb33a75f0
son-inchang <3721cha21.11@gmail.com>
2025-07-04 13:03:35 +0900

    [feat] 프로그래머스_HEAP_03_java

---
+++ programmers/HEAP/이중우선순위큐.java
@@ -0,0 +1,40 @@
+package com.kt.aicare.common.util.programmers.HEAP;
+import java.util.*;
+
+public class 이중우선순위큐 {
+  public int[] solution(String[] operations) {
+    int[] answer = new int[2];
+    PriorityQueue<Integer> maxValues = new PriorityQueue<>((n1, n2) -> (n2 - n1));
+    PriorityQueue<Integer> minValues = new PriorityQueue<>((n1, n2) -> (n1 - n2));
+
+    for (String operation : operations) {
+      String[] splited = operation.split(" ");
+      String option = splited[0];
+      int value = Integer.parseInt(splited[1]);
+      if ("D".equals(splited[0])) {
+        if (maxValues.isEmpty()) {
+          continue;
+        }
+        Integer target;
+        if (value == 1) {// 최댓값 삭제
+          target = maxValues.poll();
+        } else {// 최솟값 삭제
+          target = minValues.poll();
+        }
+        maxValues.remove(target);
+        minValues.remove(target);
+      } else {// 값 삽입
+        minValues.offer(value);
+        maxValues.offer(value);
+      }
+    }
+
+    if (!maxValues.isEmpty()) {
+      answer[0] = maxValues.poll();
+      answer[1] = minValues.poll();
+    }
+
+    return answer;
+  }
+
+}
